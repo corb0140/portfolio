a626e5c9f2674c07f3463fd6208be2295481aa7e
Mark Corbin <markpc1608@gmail.com>
2024-07-14 09:50:38 -0400

    responsive design for about and banner sections

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -60,45 +60,48 @@ const About = () => {
   };
 
   return (
-    <section id="about" className="h-auto py-5 px-10 flex flex-col">
+    <section id="about" className="h-auto py-5 px-10 lg:px-72 flex flex-col">
       <SectionTitle number="01" title="About Me" />
 
-      <p className="text-sm md:text-[1rem] tracking-tight pb-5">
-        Hey! My name is Mark. As a full-stack developer currently enrolled at
-        Algonquin college, I am passionate about blending creativity with
-        technology. With a strong foundation in front-end and back-end
-        development, I’m adept at crafting seamless digital experiences. I
-        thrive in team environments, communicate effectively, and love tackling
-        meaningful projects. Eager to keep learning and making great strides
-        through technology.
-        <br />
-        <br />
-        When I&apos;m not coding, I enjoy playing video games, watching TV, and
-        bowling.
-      </p>
+      <div className="lg:flex lg:pt-10 gap-x-5">
+        <p className="text-sm md:text-[1rem] tracking-tight  leading-7 pb-5 lg:w-[55%]">
+          Hey! My name is Mark. As a full-stack developer currently enrolled at
+          Algonquin college, I am passionate about blending creativity with
+          technology. With a strong foundation in front-end and back-end
+          development, I’m adept at crafting seamless digital experiences. I
+          thrive in team environments, communicate effectively, and love
+          tackling meaningful projects. Eager to keep learning and making great
+          strides through technology.
+          <br />
+          <br />
+          When I&apos;m not coding, I enjoy playing video games, watching TV,
+          and bowling.
+        </p>
 
-      <div
-        onMouseEnter={mouseEnter}
-        onMouseLeave={mouseLeave}
-        ref={scope}
-        className="relative h-64 w-64 self-center"
-      >
-        <div id="image" className="absolute h-full w-full  z-10">
-          <div
-            id="imageOverlay"
-            className="absolute h-full w-full opacity-70 z-10 rounded-md bg-secondary"
-          ></div>
-          <Image
-            src={me}
-            alt="head shot of my face"
-            className="rounded-md h-full w-full"
-            style={{ objectFit: "cover" }}
-          />
-        </div>
         <div
-          id="border"
-          className="absolute top-2 left-2 h-full w-full rounded-md border-2 border-secondary"
-        ></div>
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          ref={scope}
+          className="relative h-64 w-64 lg:h-72 lg:w-72 self-center lg:self-start"
+        >
+          <div id="image" className="absolute h-full w-full z-10">
+            <div
+              id="imageOverlay"
+              className="absolute h-full w-full opacity-70 z-10 rounded-md bg-secondary"
+            ></div>
+            <Image
+              src={me}
+              alt="head shot of my face"
+              className="rounded-md h-full w-full"
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+
+          <div
+            id="border"
+            className="absolute top-2 left-2 h-full w-full rounded-md border-2 border-secondary"
+          ></div>
+        </div>
       </div>
     </section>
   );
--- app/components/Banner/Banner.js
+++ app/components/Banner/Banner.js
@@ -5,16 +5,16 @@ const Banner = () => {
   return (
     <section
       id="banner"
-      className="banner h-screen w-full px-10 flex flex-col justify-center"
+      className="banner h-screen w-full px-10 lg:px-64 flex flex-col justify-center"
     >
       <p className="text-secondary pb-6">Hi, my name is</p>
-      <p className="text-4xl md:text-6xl font-bold font-Roboto tracking-wide pb-2">
+      <p className="text-4xl md:text-6xl lg:text-8xl font-bold font-Roboto tracking-wide pb-2">
         Mark Corbin.
       </p>
-      <p className="text-2xl md:text-5xl font-bold font-Roboto tracking-wide text-gray-300 pb-10">
+      <p className="text-2xl md:text-5xl lg:text-6xl font-bold font-Roboto tracking-wide text-gray-300 pb-10">
         I am a full-stack developer.
       </p>
-      <p className="text-gray-300 text-[1rem] tracking-tighter">
+      <p className="text-gray-300 text-[1rem] tracking-tighter lg:w-[60%]">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quae
         sunt a quidem, nemo aperiam corporis ut provident! Cupiditate asperiores
         accusamus natus tempore autem veritatis dolore molestiae quis neque

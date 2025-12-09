7a1a5cd26583dd8bd6b1451fd23d9dc657b22b01
corb0140 <corb0140@algonquinlive.com>
2025-12-09 09:47:52 -0500

    updated socials

--- app/data/Socials.js
+++ app/data/Socials.js
@@ -5,14 +5,14 @@ export const socials = [
     icon: <Icon icon="mingcute:github-line" />,
     link: "https://github.com/corb0140",
   },
-  {
-    icon: <Icon icon="uil:linkedin" />,
-    link: "https://www.linkedin.com/in/mark-corbin-18771b9b/",
-  },
-  {
-    icon: <Icon icon="simple-icons:indeed" />,
-    link: "https://profile.indeed.com/",
-  },
+  // {
+  //   icon: <Icon icon="uil:linkedin" />,
+  //   link: "https://www.linkedin.com/in/mark-corbin-18771b9b/",
+  // },
+  // {
+  //   icon: <Icon icon="simple-icons:indeed" />,
+  //   link: "https://profile.indeed.com/",
+  // },
   {
     icon: <Icon icon="ri:twitter-x-line" />,
     link: "https://x.com/MCorbin1608",
